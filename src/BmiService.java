02cf7c8fa8d83fe46d36ab62696570a579a7cda5
Perekatee <ivan.vedyaykin@yandex.ru>
2024-02-09 18:33:08 +0300

    first

---
+++ src/BmiService.java
@@ -0,0 +1,11 @@
+public class BmiService {
+
+    public float calculate(int kilo, float metres) {
+
+        float index;
+
+        index = kilo/(metres*metres);
+        return (int) index;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int kilo = 98;
+        float metres = 1.87f;
+        float index = service.calculate (kilo,metres);
+        System.out.println(index);
+
+    }
+}
